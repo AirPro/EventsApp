f9b6f6eb19090a7ae7192f08f585b2383d51cd1c
Bob Freid <freidrd@mail.uc.edu>
2021-01-04 18:34:03 -0500

    Setting the test using Mochito

--- src/test/java/com/event/enterpirse/EnterpriseApplicationTests.java
+++ src/test/java/com/event/enterpirse/EnterpriseApplicationTests.java
@@ -1,10 +1,13 @@
 package com.event.enterpirse;
 
+import com.event.enterpirse.dao.IPersonDAO;
 import com.event.enterpirse.dto.Person;
 import com.event.enterpirse.service.IPersonService;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -14,6 +17,9 @@ public class EnterpriseApplicationTests {
     @Autowired
     private IPersonService personService;
     private Person person;
+
+    @MockBean
+    private IPersonDAO personDAO;
 
     @Test
     void conextLoads(){
@@ -28,6 +34,7 @@ public class EnterpriseApplicationTests {
     }
 
     private void givenPersonDataAreAvailable() {
+        Mockito.when(personDAO.save(person)).thenReturn(person);
     }
 
     private void whenSearchPersonWithId1() {
