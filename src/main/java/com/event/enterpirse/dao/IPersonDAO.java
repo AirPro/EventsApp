25ebdfe223727cc82d777ee8f784ed3071d97115
Bob Freid <freidrd@mail.uc.edu>
2021-01-05 10:30:26 -0500

    Adding more comments

--- src/main/java/com/event/enterpirse/dao/IPersonDAO.java
+++ src/main/java/com/event/enterpirse/dao/IPersonDAO.java
@@ -2,6 +2,9 @@ package com.event.enterpirse.dao;
 
 import com.event.enterpirse.dto.Person;
 
+/**
+ * interface with methods
+ */
 public interface IPersonDAO {
     Person save(Person person) throws Exception;
 }
