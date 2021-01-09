881bacd4113118b64d2b15cf1300e9f0bc7f0e10
Bob Freid <freidrd@mail.uc.edu>
2021-01-08 20:35:38 -0500

    Added comments to the controller of all the endpoints.

--- src/main/java/com/event/enterpirse/EventsApplicationController.java
+++ src/main/java/com/event/enterpirse/EventsApplicationController.java
@@ -18,18 +18,37 @@ public class EventsApplicationController {
     @Autowired
     IPersonService personService;
 
+    /**
+     * Handle the root (/) endpoint and return a start page.
+     * @return
+     */
     @RequestMapping("/")
     public String index() {
         personService.fetchAll();
         return "start";
     }
 
+    /**
+     * Handle returning all Person in the database
+     * @return
+     */
     @GetMapping("/person")
     @ResponseBody
     public List<Person> fetchAllPersons() {
         return personService.fetchAll();
     }
 
+    /**
+     * Fetch a person with the given ID.
+     *
+     * Given the parameter id, find a person that corresponds to this unique ID.
+     *
+     * Returns one of the following status codes:
+     * 200: person found
+     * 400: person not found
+     *
+     * @param id a unique identifier for this person
+     */
     @GetMapping("/person/id/")
     public ResponseEntity fetchPersonById(@PathVariable("id") String id) {
         Person foundPerson = personService.fetchById(Integer.parseInt(id));
@@ -38,6 +57,16 @@ public class EventsApplicationController {
         return new ResponseEntity(foundPerson, HttpStatus.OK);
     }
 
+    /**
+     * Create a new person object, given the data provided.
+     *
+     * returns one of the following status codes:
+     * 201: successfully created a new person.
+     * 409: unable to create a person, because it already exists.
+     *
+     * @param person a JSON representation of a person object.
+     * @return the newly created person object.
+     */
     @PostMapping(value="/person", consumes="application/jason", produces="application/jason")
     public Person createPerson(@RequestBody Person person) throws Exception {
         Person newPerson = null;
@@ -49,6 +78,11 @@ public class EventsApplicationController {
         return newPerson;
     }
 
+    /**
+     * Delete a person from the database
+     * @param id
+     * @return
+     */
     @DeleteMapping("/person.id/")
     public ResponseEntity deletePerson(@PathVariable("id") String id) {
         try {
