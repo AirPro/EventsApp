b81c6060970c4aa59ce1ef1a7d3d7de257e2ff11
Bob Freid <freidrd@mail.uc.edu>
2021-04-28 21:03:25 -0400

    Added SerializedName with Maven dependency to the Person class

--- src/main/java/com/event/enterpirse/dto/Person.java
+++ src/main/java/com/event/enterpirse/dto/Person.java
@@ -1,5 +1,6 @@
 package com.event.enterpirse.dto;
 
+import com.google.gson.annotations.SerializedName;
 import lombok.Data;
 
 /**
@@ -9,14 +10,24 @@ import lombok.Data;
 public @Data
 class Person {
 
+    @SerializedName("personId")
     private int personId;
+    @SerializedName("firstName")
     String firstName;
+    @SerializedName("lastName")
     String lastName;
+    @SerializedName("address")
     String address;
+    @SerializedName("homePhone")
     String homePhone;
+    @SerializedName("cellPhone")
     String cellPhone;
+    @SerializedName("businessPhone")
     String businessPhone;
+    @SerializedName("personalEmail")
     String personalEmail;
+    @SerializedName("businessEmail")
     String businessEmail;
+    @SerializedName("businessName")
     String businessName;
 }
